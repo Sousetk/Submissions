c7885fa6a4c436168a6d036f70d07334314153d1
sousetk <salam.kabbani@yahoo.com>
2019-05-20 14:59:06 +0300

    finish step7

---
+++ Exercises/Javascript-Basics-01/step7.js
@@ -0,0 +1,10 @@
+var size = parseInt(prompt("insert ur shoe size"));
+var birth = parseInt(prompt("insert ur birth year"));
+
+var task= size*2
+task+=5
+task*=50
+task-=birth
+task+=1766
+
+alert("result" + task);
