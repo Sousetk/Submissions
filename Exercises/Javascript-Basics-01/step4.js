e99c810a989f7bfa7a04817ca3e95a0106b51000
sousetk <salam.kabbani@yahoo.com>
2019-05-20 12:10:19 +0300

    finish step4

---
+++ Exercises/Javascript-Basics-01/step4.js
@@ -0,0 +1,5 @@
+
+var name = prompt("what's ur name");
+var surname = prompt("what's ur surname");
+var city = prompt("what's ur city");
+alert("Name:" + name + "\nSurname:" + surname + "\nCity:" +city);
